28cbeea9be49d568542a01016c6015b7e36babd7
josephakeni <josephakeni@gmail.com>
2021-04-05 21:15:01 +0100

    renamed template file to templates

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,12 @@ class kubernetes {
 	package { ['kubeadm', 'kubelet', 'kubectl']:
 		ensure => 'present'
 	}
+	file { '/etc/sysctl.d/kubernetes.conf':
+        ensure => file,
+        content => template('k8_module/conf.erb'),
+    }
 	file { '/etc/yum.repos.d/kubernetes.repo':
         ensure => file,
         content => template('k8_module/repo.erb'),
-
     }
 }

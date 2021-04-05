8310866b2ee40131918837ccee039aa1c6ef3dac
josephakeni <josephakeni@gmail.com>
2021-04-05 19:28:54 +0100

    Added template file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,9 @@ class kubernetes {
 	package { ['kubeadm', 'kubelet', 'kubectl']:
 		ensure => 'present'
 	}
+	file { '/etc/yum.repos.d/kubernetes.repo':
+        ensure => file,
+        content => template('k8_module/repo.erb'),
+
+    }
 }

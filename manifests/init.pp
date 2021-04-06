50493a38d69186598165b53b604fbcb7ffa06ecf
josephakeni <josephakeni@gmail.com>
2021-04-07 00:19:58 +0100

    modified ,amifests for test env

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,31 +3,29 @@ class k8_module {
         enable => false,
         ensure => stopped,
     }
+
     exec { 'disable swap':
-      path    => ['/usr/sbin/', '/usr/bin', '/bin', '/sbin'],
-      command => 'swapoff -a',
-      unless  => "awk '{ if (NR > 1) exit 1}' /proc/swaps",
+        path    => ['/usr/sbin/', '/usr/bin', '/bin', '/sbin'],
+        command => 'swapoff -a',
+        unless  => "awk '{ if (NR > 1) exit 1}' /proc/swaps",
     }
     file_line { 'remove swap in /etc/fstab':
-      ensure            => absent,
-      path              => '/etc/fstab',
-      match             => '\sswap\s',
-      match_for_absence => true,
-      multiple          => true,
-    } 
-    class { selinux:
-        mode => 'disabled',
-        type => 'targeted',
+        ensure            => absent,
+        path              => '/etc/fstab',
+        match             => '\sswap\s',
+        match_for_absence => true,
+        multiple          => true,
     }
-	package { ['kubeadm', 'kubelet', 'kubectl']:
-		ensure => 'present'
-	}
-	file { '/etc/sysctl.d/kubernetes.conf':
+
+    file { '/etc/sysctl.d/kubernetes.conf':
         ensure => file,
         content => template('k8_module/conf.erb'),
     }
-	file { '/etc/yum.repos.d/kubernetes.repo':
+    file { '/etc/yum.repos.d/kubernetes.repo':
         ensure => file,
         content => template('k8_module/repo.erb'),
+    }
+    package { ['kubeadm', 'kubelet', 'kubectl']:
+        ensure => present,
     }
 }

181952aedf9ea2468bc19098dc4edc1844cda7f7
josephakeni <josephakeni@gmail.com>
2021-04-06 22:48:48 +0100

    modified Puppetfile for test env

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,24 @@
 class k8_module {
+    service { 'firewalld':
+        enable => false,
+        ensure => stopped,
+    }
+    exec { 'disable swap':
+      path    => ['/usr/sbin/', '/usr/bin', '/bin', '/sbin'],
+      command => 'swapoff -a',
+      unless  => "awk '{ if (NR > 1) exit 1}' /proc/swaps",
+    }
+    file_line { 'remove swap in /etc/fstab':
+      ensure            => absent,
+      path              => '/etc/fstab',
+      match             => '\sswap\s',
+      match_for_absence => true,
+      multiple          => true,
+    } 
+    class { selinux:
+        mode => 'disabled',
+        type => 'targeted',
+    }
 	package { ['kubeadm', 'kubelet', 'kubectl']:
 		ensure => 'present'
 	}
